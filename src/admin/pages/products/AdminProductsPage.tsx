4baef614e6620fd786f9ce3beae6ec0133a5f8c5
Fredy Aristizabal <freddyarh13@gmail.com>
2025-09-16 09:36:57 -0500

    Added Custom logo and table

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,6 +1,58 @@
+import { AdminTitle } from "@/admin/components/components/AdminTitle"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { Button } from "@/components/ui/button"
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
+import { PlusIcon } from "lucide-react"
+import { Link } from "react-router"
 
 export const AdminProductsPage = () => {
   return (
-    <div>AdminProductsPage</div>
+    <>
+      <div className="flex justify-between items-center">
+        <AdminTitle title="Products" subtitle="Here you can see and manage your products." />
+      
+        <Link to="/admin/products/new">
+          <Button>
+            <PlusIcon />
+            New product
+          </Button>
+        </Link>
+      </div>
+
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Image</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Inventory</TableHead>
+            <TableHead>Sizes</TableHead>
+            <TableHead className="text-right">Accions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>
+              <img src="https://placehold.co/250*250" alt="Product" className="w-20 h-20 object-cover rounded-md" />
+            </TableCell>
+            <TableCell>Product 1</TableCell>
+            <TableCell>$250</TableCell>
+            <TableCell>Category 1</TableCell>
+            <TableCell>100 stock</TableCell>
+            <TableCell>XS,S,L</TableCell>
+            <TableCell className="text-right">
+              <Link to={`/admin/products/t-shirt-teslo`}>
+                Editar
+              </Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={10} />
+    </>
   )
 }
--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,20 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductGrid } from "@/shop/components/ProductGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+  const { gender } = useParams();
+
+  const genderLabel = gender === 'men' ? 'Hombres' : gender === 'women' ? 'Mujeres' : 'Niños';
+
   return (
-    <div>GenderPage</div>
+    <>
+      <CustomJumbotron title={`Todos los productos para ${genderLabel}`} />
+      <ProductGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
   )
 }
